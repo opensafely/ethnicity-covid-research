89e81a84e4c517a70c909fc384d99a8dc5e803ef
rohinimathur <ladyroho@gmail.com>
2020-07-07 12:01:46 +0100

    fix ventilated brackets

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -95,7 +95,8 @@ study = StudyDefinition(
 
     icu_date_ventilated=patients.ventilated_in_icu(
     on_or_after="2020-02-01",
-    returning="icu_date_ventilated")
+    returning="icu_date_ventilated"
+    )
 
     icu_date_admitted=patients.admitted_to_icu(
         on_or_after="2020-02-01",
