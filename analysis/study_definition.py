f83e897bc48b2581540ced05bb1f027409019862
rohinimathur <ladyroho@gmail.com>
2020-07-07 12:03:04 +0100

    added commas after brackets

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -91,12 +91,12 @@ study = StudyDefinition(
     icu_ventilation=patients.ventilated_in_icu(
     on_or_after="2020-02-01",
     returning="binary_flag"  
-    )
+    ),
 
     icu_date_ventilated=patients.ventilated_in_icu(
     on_or_after="2020-02-01",
     returning="icu_date_ventilated"
-    )
+    ),
 
     icu_date_admitted=patients.admitted_to_icu(
         on_or_after="2020-02-01",
