2b61d9f08456246697dddee28c2c23d92e54927c
rohinimathur <ladyroho@gmail.com>
2020-07-13 17:18:00 +0100

    updated ethnicity ratios

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -267,7 +267,7 @@ study = StudyDefinition(
         find_last_match_in_period=True,
         include_date_of_match=True,
         return_expectations={
-            "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
+            "category": {"ratios": {"1": 0.8, "2":0.05, "3":0.05, "4":0.05, "5": 0.05}},
             "incidence": 0.75,
         },
     ),
