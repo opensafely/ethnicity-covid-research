f17af397076415edc4e7bde827ba2bdeda9c5a3a
rohinimathur <ladyroho@gmail.com>
2020-09-02 19:33:18 +0100

    add prison flag to study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -347,6 +347,11 @@ study = StudyDefinition(
         }
     ),
 
+     prison_flag=patients.household_status_as_of(
+    "2020-02-01",
+    returning="is prison",
+),
+
     ### GP CONSULTATION RATE IN 12 MONTH BEFORE FEB 1 2020
     gp_consult_count=patients.with_gp_consultations(
         between=["2019-02-01", "2020-01-31"],
@@ -871,6 +876,8 @@ study = StudyDefinition(
             "date": {"earliest": "2019-02-01", "latest": "2020-01-31"}
         },
     ),
+
+
 
 
 )
